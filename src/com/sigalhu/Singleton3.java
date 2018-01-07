d636be0a95eb19d30b95c702a26e0ed3475a07be
huxujun <huxujun_i@didichuxing.com>
2018-01-07 20:58:55 +0800

    上传懒汉式单例

---
+++ src/com/sigalhu/Singleton3.java
@@ -0,0 +1,18 @@
+package com.sigalhu;
+
+public class Singleton3 {
+    private static Singleton3 singleton = null;
+
+    private Singleton3(){}
+
+    public static Singleton3 getSingleton(){
+        if(singleton == null){
+            synchronized (Singleton3.class){
+                if(singleton == null){
+                    singleton = new Singleton3();
+                }
+            }
+        }
+        return singleton;
+    }
+}
