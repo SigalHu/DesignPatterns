d9fc4302db07b698a718129eb373494cebdac9a9
huxujun <huxujun_i@didichuxing.com>
2018-01-08 15:41:35 +0800

    调整目录

---
+++ src/com/sigalhu/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package com.sigalhu.singleton;
+
+public class Singleton {
+    private static final Singleton singleton = new Singleton();
+
+    private Singleton(){}
+
+    public static Singleton getSingleton(){
+        return singleton;
+    }
+}
