23dc12638cd60c00d9f5ea1942ddf66ec03287ae
huxujun <huxujun_i@didichuxing.com>
2018-01-29 23:55:03 +0800

    上传策略模式

---
+++ src/com/sigalhu/strategypattern/ConcreteStrategy2.java
@@ -0,0 +1,7 @@
+package com.sigalhu.strategypattern;
+
+public class ConcreteStrategy2 implements Strategy {
+    @Override
+    public void doSomething() {
+    }
+}
---
+++ src/com/sigalhu/strategypattern/Strategy.java
@@ -0,0 +1,6 @@
+package com.sigalhu.strategypattern;
+
+public interface Strategy {
+    //策略模式的运算法则
+    public void doSomething();
+}
