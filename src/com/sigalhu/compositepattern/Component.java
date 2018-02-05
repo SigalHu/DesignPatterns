cf71f0e8eaa8be4b5ca0a4a028b7430832b24e39
huxujun <huxujun_i@didichuxing.com>
2018-02-05 23:41:18 +0800

    上传组合模式

---
+++ src/com/sigalhu/compositepattern/Component.java
@@ -0,0 +1,8 @@
+package com.sigalhu.compositepattern;
+
+public abstract class Component {
+    //个体和整体都具有的共享
+    public void doSomething(){
+        //业务逻辑
+    }
+}
---
+++ src/com/sigalhu/compositepattern/Leaf.java
@@ -0,0 +1,9 @@
+package com.sigalhu.compositepattern;
+
+public class Leaf extends Component {
+    //可以覆写父类方法
+    @Override
+    public void doSomething() {
+        super.doSomething();
+    }
+}
