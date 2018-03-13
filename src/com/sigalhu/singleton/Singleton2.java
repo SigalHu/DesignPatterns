f83e522c57aa0c067ab2a7c70ec4dbce0b1b8708
huxujun <huxujun_i@didichuxing.com>
2018-03-13 11:25:46 +0800

    完善单例模式

--- src/com/sigalhu/singleton/Singleton2.java
+++ src/com/sigalhu/singleton/Singleton2.java
@@ -3,8 +3,10 @@ package com.sigalhu.singleton;
 public class Singleton2 {
     private static Singleton2 singleton = null;
 
+    //限制产生多个对象
     private Singleton2(){}
 
+    //通过该方法获得实例对象
     public static Singleton2 getSingleton(){
         if(singleton == null){
             singleton = new Singleton2();
--- src/com/sigalhu/singleton/Singleton3.java
+++ src/com/sigalhu/singleton/Singleton3.java
@@ -3,8 +3,10 @@ package com.sigalhu.singleton;
 public class Singleton3 {
     private static Singleton3 singleton = null;
 
+    //限制产生多个对象
     private Singleton3(){}
 
+    //通过该方法获得实例对象
     public static Singleton3 getSingleton(){
         if(singleton == null){
             synchronized (Singleton3.class){
