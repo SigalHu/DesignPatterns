c0b2d475918c8f1e187cc12e18a4662565f605b1
huxujun <huxujun_i@didichuxing.com>
2018-05-04 21:51:19 +0800

    完善适配器模式

--- src/com/sigalhu/adapterpattern/Adaptee.java
+++ src/com/sigalhu/adapterpattern/Adaptee.java
@@ -3,5 +3,6 @@ package com.sigalhu.adapterpattern;
 public class Adaptee {
     //原有的业务逻辑
     public void doSomething(){
+        System.out.println("I'm kind of busy, leave me alone, pls!");
     }
 }
--- src/com/sigalhu/adapterpattern/ConcreteTarget.java
+++ src/com/sigalhu/adapterpattern/ConcreteTarget.java
@@ -3,5 +3,6 @@ package com.sigalhu.adapterpattern;
 public class ConcreteTarget implements Target {
     @Override
     public void request() {
+        System.out.println("if you need any help, pls call me!");
     }
 }
